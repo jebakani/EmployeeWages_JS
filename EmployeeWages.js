99bfc02bd5894169268c88e5754a7509e73d306c
Jebakani <vjishwarya@gmail.com>
2021-08-12 10:55:49 +0530

    [JEBAKANI] Adding : Checking the employee attendance

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -1,1 +1,13 @@
 console.log("Welcome to EmployeeWages");
+//UC1-Check Employee Present or Absent
+const IS_PRESENT=1;
+let empCheck=Math.floor(Math.random()*10)%2;
+if(empCheck==IS_PRESENT)
+{ 
+    console.log("UC1-Employee Present");
+    return ;
+}
+else
+{
+    console.log("UC1-Employee Absent");
+}
